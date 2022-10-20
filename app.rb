1c48900fcecffc1c98f732af13bdac83848ee96a
Joanne Imbisi <evizengwa@yahoo.com>
2022-10-20 14:41:57 -0700

    commit tree 59344abf27558cb4091bfd85239dc9d73fbd49fb

---
+++ app.rb
@@ -0,0 +1,10 @@
+#This is how to comment in Ruby
+#puts is how to console.log --> Great for strings
+#p method will output the data in a nicer format by calling the .inspect method on our data.
+#pp "pretty-printing" complex data, such as nested arrays and hashes (the Ruby equivalent of a JavaScript object)- by calling the .pretty_inspect method
+#nil is similar to null(no value)
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
+
